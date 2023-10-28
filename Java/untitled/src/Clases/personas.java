0c1a07df742ee0e49372d34c6204731dcfe84fa5
Gineeesss <ggutierrezs01@iescastelar.com>
2023-10-28 13:25:00 +0200

    Good luck

---
+++ Java/untitled/src/Clases/personas.java
@@ -0,0 +1,18 @@
+package Clases;
+
+public class personas {
+    private String nombre;
+    private String genero;
+    private float edad;
+    private float peso;
+    private float recorrido;
+public void nacer(String nombre,String genero,float edad,float peso,float recorrido){
+    this.nombre=nombre;
+    this.genero=genero;
+    this.edad=edad;
+    this.peso=peso;
+    this.recorrido=recorrido;
+}
+
+
+}
